4b333fb64806f5aaac3f03c3446cf5fb614e8908
shuki <shukiheftler@gmail.com>
2025-06-18 15:15:36 +0300

    finish + database

--- sensors/GameFlow/Program.cs
+++ sensors/GameFlow/Program.cs
@@ -13,53 +13,9 @@ namespace sensors
         {
             InterrogationRoom room = new InterrogationRoom();
             room.interrogat();
-            //FireWorks fireWorks = new FireWorks();
             
-            VictoryShimmerEffect();
-            Console.WriteLine("\nPress any key to exit...");
-            Console.ReadKey();
-            
-
-            void VictoryShimmerEffect()
-            {
-                string victoryMessage = "~~ YOU WIN! ~~";
-                ConsoleColor[] colors = new ConsoleColor[]
-                {
-                ConsoleColor.Yellow,
-                ConsoleColor.Green,
-                ConsoleColor.Cyan,
-                ConsoleColor.Magenta,
-                ConsoleColor.White
-                };
-
-                for (int i = 0; i < 10; i++)
-                {
-                    Console.Clear();
-
-                    // צבע משתנה לפי איטרציה
-                    Console.ForegroundColor = colors[i % colors.Length];
-
-                    // הדפסה של כוכבים בצדדים עם אפקט "זוהר"
-                    string stars = new string('*', i % 6 + 1);
-                    Console.WriteLine($"{stars} {victoryMessage} {stars}");
-
-                    Thread.Sleep(300); // השהייה להדגשת האפקט
-                }
-
-                // אחרי הלולאה, הדפס את ההודעה בצבע זהב קבוע
-                Console.Clear();
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"***** {victoryMessage} *****");
-                Console.ResetColor();
             }
         }
-        //var s1 = SensorService.CreateAudioSensor();
-        //var s2 = SensorService.CreateAudioSensor();
-
-        //Console.WriteLine(s1.Name == s2.Name);
-        //Console.WriteLine(s1 == s2); // ➜ False
-        //Console.WriteLine(ReferenceEquals(s1, s2)); // ➜ F
-    }
 
     }
-//}
+
